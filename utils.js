82fcf1be37c4773a4b10f706dae0e30794a7afaf
Thomas Johnson <thomasij813@gmail.com>
2019-10-01 12:17:47 -0700

    utility for recursively turning json data into nested array

---
+++ utils.js
@@ -0,0 +1,37 @@
+const parseJsonIntoArray = (json) => {
+    let headers = {};
+    let rows = [];
+    let i = 0;
+
+    const recurse = (root) => {
+        const localHeaders = Object.keys(root);
+
+        let row = localHeaders
+            .filter(header => header !== 'children')
+            .reduce((row, header) => {
+                if (!headers.hasOwnProperty(header)) {
+                    headers[header] = i;
+                    i++;
+                }
+                let headerIndex = headers[header];
+                row[headerIndex] = root[header];
+                return row;
+            }, []);
+
+        rows.push(row);
+
+        if (root.children.length > 0) {
+            root.children.forEach(recurse);
+        }
+    };
+    
+    recurse(json);
+
+    const firstRow = Object.keys(headers).reduce((row, header) => {
+        row.push(header)
+        return row;
+    }, [])
+    rows.unshift(firstRow);
+    
+    return rows;
+}
