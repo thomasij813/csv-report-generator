85561cb76d489168c7ab6bd620ff0dd1543f618c
Thomas Johnson <thomasij813@gmail.com>
2019-10-01 12:17:09 -0700

    handle submitting posting of form data to server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('tiny'));
 
+app.use(bodyParser.urlencoded());
 app.use('/', express.static('client'));
+
+
+app.post('/report', (req, res) => {
+    let f = req.body.json_data;
+    console.log(f);
+    res.redirect('/');
+})
 
 const port = 3000;
 
